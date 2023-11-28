fe750076bfb8df8af60f642b648095d94fcd9de8
xiaoqj <xiaoqj@sz.bwoil.com>
2023-11-28 20:12:49 +0800

    add cas simple list

---
+++ common/cas_slist.h
@@ -0,0 +1,9 @@
+/*----------------------------------------------------------------
+// Copyright 2021
+// All rights reserved.
+//
+// author: 404558965@qq.com (xiaoquanjie)
+// github: https://github.com/xiaoquanjie/cgo
+// Created by xiaoqj on 2023/11/28
+//----------------------------------------------------------------*/
+
