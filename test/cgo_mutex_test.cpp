def5b6ad998ae4fc94f56e368048800d0780ca71
xiaoqj <xiaoqj@sz.bwoil.com>
2024-05-11 17:29:25 +0800

    添加测试用例

--- test/cgo_mutex_test.cpp
+++ test/cgo_mutex_test.cpp
@@ -4,6 +4,8 @@
 
 #include <gtest/gtest.h>
 #include <atomic>
+#include <thread>
+#include <list>
 #include "print.h"
 
 TEST(cgo_mutex_test, 1) {
@@ -13,6 +15,7 @@ TEST(cgo_mutex_test, 1) {
     int loop_count = 1000000;
     int count = 0;
 
+    // golang的基准测试才花200多毫秒
     for (int i = 0 ; i < loop_count; i++) {
         wg.Add(1);
         // 这里性能表现不好.需要排查一下原因,可能是因为内存分配的原因.
@@ -20,6 +23,60 @@ TEST(cgo_mutex_test, 1) {
             mu.lock();
             count++;
             mu.unlock();
+            wg.Done();
+        };
+    }
+
+    wg.Wait();
+    EXPECT_EQ(loop_count, count);
+}
+
+TEST(standard_mutex_test, 1) {
+    int loop_count = 1000000;
+    int count = 0;
+
+    std::mutex mu;
+    std::list<std::thread> thrs;
+    for (int i = 0; i < std::thread::hardware_concurrency(); i++) {
+        thrs.push_back(std::move(std::thread([&mu, &count, loop_count]{
+            while (true) {
+                mu.lock();
+                if (count == loop_count) {
+                    mu.unlock();
+                    break;
+                }
+                count++;
+                mu.unlock();
+            }
+        })));
+    }
+
+    for (auto& th : thrs) {
+        th.join();
+    }
+
+    EXPECT_EQ(loop_count, count);
+}
+
+TEST(cgo_mutex_test, 2) {
+    int loop_count = 1000000;
+    int count = 0;
+
+    cgo::mutex mu;
+    cgo::WaitGroup wg;
+
+    for (int i = 0; i < std::thread::hardware_concurrency(); i++) {
+        wg.Add(1);
+        go [&mu, &count, loop_count, &wg] {
+            while (true) {
+                mu.lock();
+                if (count == loop_count) {
+                    mu.unlock();
+                    break;
+                }
+                count++;
+                mu.unlock();
+            }
             wg.Done();
         };
     }
